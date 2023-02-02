dbd9c3f3a601f21e06997a9c79a1fb2c129ff69f
khagesh1998 <khagesh.kumar@dream11.com>
2023-02-02 15:27:57 +0530

    first commit

---
+++ pixel7/index.js
@@ -0,0 +1,7 @@
+
+$(document).ready(function () {
+  // console.log("Hello World!");
+  $('#userAgent').text(navigator.userAgent)
+  $('#platform').text(navigator.platform)
+  // console.log(navigator)
+});
