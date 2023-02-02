6149b183b69a5fa84ab70e8f94cd633a73f7e980
khagesh1998 <khagesh.kumar@dream11.com>
2023-02-02 17:14:57 +0530

    chore: remove button

--- pixel7/index.js
+++ pixel7/index.js
@@ -1,15 +1,46 @@
 
+
 $(document).ready(function () {
+  const writeToClipboard =  async (value) => {
+    document.execCommand("copy")
+    // alert("copy")
+    // navigator.permissions.query({name: "clipboard-write"}).then((result) => {
+    //   if (result.state === "granted" || result.state === "prompt") {
+    //     /* write to the clipboard now */
+    //     navigator.clipboard.writeText(value).then(() => {
+    //       alert("successfully copied");
+    //     }).catch(() => {
+    //         alert("something went wrong");
+    //       })
+    //   }
+    // });
+    navigator.clipboard.writeText(value)
+      .then(() => {
+        alert("successfully copied");
+      }).catch(() => {
+        alert("something went wrong");
+      })
+
+    // try {
+    //   const x = await navigator.clipboard.writeText("ljljlkjlk");
+    //   console.log('Page URL copied to clipboard', x, navigator.clipboard);
+    // } catch (err) {
+    //   console.error('Failed to copy: ', err);
+    // }
+
+    // document.execCommand("copy")
+  }
+
   // console.log("Hello World!");
   $('#userAgent').text(navigator.userAgent)
   // $('#platform').text(navigator.platform)
   // console.log(navigator)
 
   $('#userAgentButton').click(()=>{
-    navigator.clipboard.writeText(navigator.userAgent);
+    writeToClipboard(navigator.userAgent)
   })
 
   $('#urlButton').click(()=>{
-    navigator.clipboard.writeText("https://khagesh1998.github.io/pixel7/");
+    writeToClipboard("https://khagesh1998.github.io/pixel7/pixel7")
   })
 });
