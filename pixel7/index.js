c2c849e6b9c93ceb02482f6bdca509f5f72dd6d4
khagesh1998 <khagesh.kumar@dream11.com>
2023-02-02 16:14:52 +0530

    feat: copy to clipboard

--- pixel7/index.js
+++ pixel7/index.js
@@ -4,4 +4,12 @@ $(document).ready(function () {
   $('#userAgent').text(navigator.userAgent)
   // $('#platform').text(navigator.platform)
   // console.log(navigator)
+
+  $('#userAgentButton').click(()=>{
+    navigator.clipboard.writeText(navigator.userAgent);
+  })
+
+  $('#urlButton').click(()=>{
+    navigator.clipboard.writeText("https://khagesh1998.github.io/pixel7/");
+  })
 });
